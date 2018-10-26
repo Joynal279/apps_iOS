5c0a52b53a446460a7109920302c6bfe0ef52008
Joynal279 <44470728+Joynal279@users.noreply.github.com>
2018-10-27 04:07:09 +0600

    Home work

---
+++ image5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  image5
+//
+//  Created by MacBook Air on 25/10/18.
+//  Copyright © 2018 MCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ image5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  image5
+//
+//  Created by MacBook Air on 25/10/18.
+//  Copyright © 2018 MCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
